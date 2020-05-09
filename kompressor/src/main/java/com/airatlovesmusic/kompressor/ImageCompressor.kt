4dbdfd8f43f9ded473d500512fedd23af76b9e5a
Airat Khalilov <airat@rcsmail.ru>
2020-05-09 14:42:29 +0300

    0.1.2

--- kompressor/src/main/java/com/airatlovesmusic/kompressor/ImageCompressor.kt
+++ kompressor/src/main/java/com/airatlovesmusic/kompressor/ImageCompressor.kt
@@ -3,6 +3,8 @@ package com.airatlovesmusic.kompressor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import com.airatlovesmusic.kompressor.steps.*
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 
@@ -14,17 +16,18 @@ class ImageCompressor {
 
     /***
      * @param inputStream [InputStream] - any type of inputStreams from which lib can read needed data
-     * @param outputStream [OutputStream] - any type of outputStreams to which lib can save compressed data
+     * @return [OutputStream] - any type of outputStreams to which lib can save compressed data
      */
     fun compress (
         inputStream: InputStream,
-        outputStream: OutputStream,
         format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG,
         quality: Int = 80,
         steps: CompressionStep = defaultSteps(inputStream)
-    ) {
+    ): InputStream {
+        val baos = ByteArrayOutputStream()
         val result = steps.invoke(BitmapFactory.decodeStream(inputStream))
-        result.compress(format, quality, outputStream)
+        result.compress(format, quality, baos)
+        return ByteArrayInputStream(baos.toByteArray())
     }
 
     private fun defaultSteps(inputStream: InputStream): CompressionStep =
